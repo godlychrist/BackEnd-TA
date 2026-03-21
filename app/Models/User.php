15e25c323cf70e9d428786e8f953409fdbd5e8c7
Cristopher <cristopherquiros2@gmail.com>
2026-03-20 18:43:24 -0600

    feat: added cedula

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,7 @@ class User extends Authenticatable implements JWTSubject
     protected $collection = 'users';
 
     protected $fillable = [
+        'cedula',
         'username',
         'password',
     ];
